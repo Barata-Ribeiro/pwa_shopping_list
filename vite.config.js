e09a8d0d60d860e171c73d04353461cc7199bc07
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2023-08-05 07:55:21 -0300

    chore: config pwa

--- vite.config.js
+++ vite.config.js
@@ -13,6 +13,40 @@ export default defineConfig({
       cache: false,
       fix: true,
     }),
-    VitePWA({ registerType: 'autoUpdate' }),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      manifest: {
+        name: 'My Shopping List',
+        short_name: 'Shopping List',
+        description:
+          'A PWA shopping list to help you with remembering what you need to buy.',
+        theme_color: '#f0f1f5',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+        ],
+      },
+    }),
   ],
 });
