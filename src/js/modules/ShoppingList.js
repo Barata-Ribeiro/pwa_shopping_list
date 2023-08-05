5c54274e15e7a00cbadb4e4d4877fcafa5fd3c0d
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2023-08-04 21:01:03 -0300

    feat: add addTo/deleteFrom methods to shopping list

--- src/js/modules/ShoppingList.js
+++ src/js/modules/ShoppingList.js
@@ -11,14 +11,17 @@ class ShoppingList {
     this.errorMessageElement = document.querySelector('#error-message');
     this.shoppingListElement = document.querySelector('#shopping-list');
 
+    // Firebase Properties
     this.firebaseConfig = {
       databaseURL:
         'https://shopping-cart-list-f1b57-default-rtdb.firebaseio.com/',
     };
-
     this.firebaseApp = initializeApp(this.firebaseConfig);
     this.firebaseDatabase = getDatabase(this.firebaseApp);
     this.firebaseShoppingListRef = ref(this.firebaseDatabase, 'shoppingList');
+
+    this.bindMethods();
+    this.addEventListeners();
   }
   // Methods to Create
   createShoppingListElement(value, key) {
@@ -42,6 +45,8 @@ class ShoppingList {
       this.shoppingListElement.appendChild(emptyElement);
       this.shoppingListElement.style.display = 'none';
     }
+
+    this.clearShoppingListElement();
   }
 
   // Methods to Clear/Delete
@@ -53,10 +58,57 @@ class ShoppingList {
     this.shoppingListElement.innerHTML = '';
   }
 
-  // Methods to Render List
+  clearErrorMessageElement() {
+    this.errorMessageElement.textContent = '';
+    this.errorMessageElement.style.display = 'none';
+  }
 
-  // Events
-  addEventListeners() {}
+  deleteItemsFromShoppingList(event) {
+    event.preventDefault();
+
+    if (event.target && event.target.nodeName === 'li') {
+      let locationOfItemInDB = ref(
+        this.firebaseDatabase,
+        `shoppingList/${event.target.id}`,
+      );
+
+      remove(locationOfItemInDB);
+    }
+  }
+
+  // Methods to Render List
+  addItemsToShoppingList(event) {
+    event.preventDefault();
+
+    let inputValue = this.inputFieldEl.value.trim();
+
+    if (inputValue === '') {
+      this.clearErrorMessageElement();
+      this.errorMessageElement.style.display = 'block';
+      this.errorMessageElement.textContent = 'Please enter a value';
+      return;
+    }
+
+    this.clearErrorMessageElement();
+    push(this.firebaseShoppingListRef, inputValue);
+    this.clearInputFieldElement();
+  }
+
+  // Events and Bindings
+  bindMethods() {
+    this.clearErrorMessageElement = this.clearErrorMessageElement.bind(this);
+  }
+
+  addEventListeners() {
+    this.addButtonElement.addEventListener(
+      'click',
+      this.addItemsToShoppingList,
+    );
+    this.inputFieldElement.addEventListener(
+      'input',
+      this.clearErrorMessageElement,
+    );
+  }
 }
 
 export default ShoppingList;
